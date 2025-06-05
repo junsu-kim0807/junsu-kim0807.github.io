3833cf0ede2bd1fecd70c8e54392c1ac4c19a131
junsu-kim0807 <56712500+junsu-kim0807@users.noreply.github.com>
2025-06-05 07:37:45 +0000

    Deploying to gh-pages from @ junsu-kim0807/junsu-kim0807.github.io@028ea1aa9ec0a57e9001fdd6c1d7821a81ad9325 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,11 +10,11 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/CV.pdf";
           },
         },{id: "nav-publications",
           title: "Publications",
